e2f8d2543dd4abc7f119490f8aef665a6cd1792a
Your Name <you@example.com>
2023-02-14 04:48:04 +0100

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,12 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
 import logo from '../assets/img/euphy2.png';
 import navIcon1 from "../assets/img/nav-icon1.svg";
 
 export const Footer = () => {
   return (
     <footer className="footer">
-      <Container>
+      <Container style={{paddingTop:"20px"}}>
         <Row className="align-items-center">
-          <MailchimpForm />
           <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
