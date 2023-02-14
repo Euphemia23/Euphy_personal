48049cb401a0f81d86b05a3f7a7a3d52633e0e4a
Your Name <you@example.com>
2023-02-14 04:28:27 +0100

    Initial Commit

---
+++ src/components/Banner.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/headerimg.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [, setIndex] = useState(1);
+  const toRotate = [  "Computer Engineer", "Python Developer", "FullStack Developer" ];
+  const period = 2000;
+
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <h1>{`Hi! I'm Euphemia`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Computer Engineer", "Machine Learning Engineer", "Backend Developer" ]'><span className="wrap">{text}</span></span></h1>
+                <p> My name is Euphemia Agwa, I am a Computer Engineer, Python  and FullStack Developer. I started my career as an Instrumentation and control engineer after my Electronics/Computer Science degree. I transitioned into software development during my masters degree in Electrical, Electronics and Computer Engineering. I am passionate about building scalable and efficient software solutions. I am also a strong advocate for the use of technology to solve real world problems. I am currently looking for opportunities to work with like minded individuals to build amazing products.
+        </p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
